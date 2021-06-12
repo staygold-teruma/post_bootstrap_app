4d4e5e30e60226c8eac0612173f92c07d3eb549e
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-06-12 17:47:02 +0900

    編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,9 +23,13 @@ class PostsController < ApplicationController
   # IDが3の場合は「/posts/3」にリダイレクトされる
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post
   end
 
   def destroy
